25ee0ccd7af6f79af92dba38e47dfc3561d152ad
Антон Антонов <antonov_anton_88@mail.ru>
2019-11-23 18:55:17 +0300

    Backgrounds with a parallax effect

---
+++ Assets/Scripts/Game/Parallax/ParallaxController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Framework.Tools.Singleton;
+
+namespace Assets.Scripts.Game.Parallax
+{
+    public sealed class ParallaxController : MonoSingleton<ParallaxController>
+    {
+        private readonly List<ParallaxBehavior> _parallaxBehaviors = new List<ParallaxBehavior>();
+
+        public void AddBehavior(ParallaxBehavior behavior)
+        {
+            if (_parallaxBehaviors.Contains(behavior))
+            {
+                return;
+            }
+
+            _parallaxBehaviors.Add(behavior);
+        }
+
+        public void RemoveBehavior(ParallaxBehavior behavior)
+        {
+            _parallaxBehaviors.Remove(behavior);
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
